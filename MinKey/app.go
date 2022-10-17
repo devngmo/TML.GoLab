7c379a818eca6195c01ee2b84b22ee02557c615c
Tran Manh Linh <linhtm.me@gmail.com>
2022-10-17 20:42:22 +0700

    Create app MinKey
    A simple API for Key Value Pair of String like Redis, but easier to run

---
+++ MinKey/app.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var keyMap = make(map[string]string)
+
+func setKey(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	keyMap[key] = string(reqBody)
+	fmt.Println("SET " + key + ": " + keyMap[key])
+	save()
+}
+
+func getKey(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+	value, ok := keyMap[key]
+	if !ok {
+		fmt.Println("GET " + key + ": NULL")
+		fmt.Fprintf(w, "")
+	} else {
+		fmt.Println("GET " + key + ": " + value)
+		fmt.Fprintf(w, value)
+	}
+}
+
+func loadKeyMap() {
+	contentBytes, err := ioutil.ReadFile("data.txt")
+	if err == nil {
+		b := new(bytes.Buffer)
+		b.Write(contentBytes)
+		d := gob.NewDecoder(b)
+		err = d.Decode(&keyMap)
+		if err != nil {
+			panic(err)
+		} else {
+			fmt.Println("Data Loaded: ", len(keyMap), " Keys")
+			i := 1
+			for k, v := range keyMap {
+				fmt.Printf("\n%d. [%s] = [%s]", i, k, v)
+				i++
+			}
+		}
+	} else {
+		fmt.Println("Data file not found => Reset All Keys")
+	}
+}
+
+func save() {
+	b := new(bytes.Buffer)
+	e := gob.NewEncoder(b)
+	err := e.Encode(keyMap)
+	if err != nil {
+		panic(err)
+	} else {
+		ioutil.WriteFile("data.txt", b.Bytes(), 0644)
+	}
+}
+
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
+}
+
+func handleRequests() {
+	loadKeyMap()
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/string/{key}", setKey).Methods("PUT", "POST")
+	router.HandleFunc("/string/{key}", getKey).Methods("GET")
+	ip := GetOutboundIP()
+	log.Fatal(http.ListenAndServe(ip.String()+":24500", router))
+}
+
+func main() {
+	handleRequests()
+}
